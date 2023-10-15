7e8a2bc28229a1abc9bbab7e49af014b8e9d68a3
TwentyFive <TwentyFiveSoftware@gmail.com>
2023-10-15 18:07:16 +0200

    Add multisampling

--- lib/elixir_ray_tracing.ex
+++ lib/elixir_ray_tracing.ex
@@ -2,6 +2,7 @@ defmodule ElixirRayTracing do
   @width 300
   @height 200
   @max_ray_recursive_depth 50
+  @samples_per_pixel 10
 
   @aspect_ratio @width / @height
   @viewport_height 2.0
@@ -23,13 +24,20 @@ defmodule ElixirRayTracing do
 
   @spec ray_trace() :: [%Vector3{}]
   def ray_trace() do
-    for y <- 0..(@height - 1), x <- 0..(@width - 1), do: calculate_pixel_color(x, y)
+    for y <- 0..(@height - 1), x <- 0..(@width - 1) do
+      accumulated_pixel_color =
+        Enum.reduce(1..@samples_per_pixel, %Vector3{}, fn _, color ->
+          Vector3.vector_add(color, calculate_pixel_color(x, y))
+        end)
+
+      Vector3.vector_mul_scalar(accumulated_pixel_color, 1.0 / @samples_per_pixel)
+    end
   end
 
   @spec calculate_pixel_color(integer(), integer()) :: %Vector3{}
   def calculate_pixel_color(x, y) do
-    u = x / (@width - 1)
-    v = y / (@height - 1)
+    u = (x + :rand.uniform()) / (@width - 1)
+    v = (y + :rand.uniform()) / (@height - 1)
 
     ray = Camera.ray_from_camera_to_uv(@camera, u, v)
     Ray.ray_color(ray, @spheres, 0, @max_ray_recursive_depth)
