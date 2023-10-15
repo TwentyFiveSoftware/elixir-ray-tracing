184671af94821b88da09cb391fb4b8eac4a3d12f
TwentyFive <TwentyFiveSoftware@gmail.com>
2023-10-15 22:00:43 +0200

    Add concurrent rendering

--- lib/elixir_ray_tracing.ex
+++ lib/elixir_ray_tracing.ex
@@ -1,6 +1,6 @@
 defmodule ElixirRayTracing do
-  @width 800
-  @height 450
+  @width 1920
+  @height 1080
   @max_ray_recursive_depth 50
   @samples_per_pixel 1
 
@@ -8,24 +8,39 @@ defmodule ElixirRayTracing do
   @scene Scene.generate_scene()
 
   def main do
-    pixels = ray_trace()
+    {render_time, pixels} = :timer.tc(ElixirRayTracing, :ray_trace, [])
+    IO.puts("rendered #{@samples_per_pixel} samples/pixel in #{render_time / 1000} ms")
+
     Image.save_as_png("./render.png", @width, @height, pixels)
   end
 
   @spec ray_trace() :: [%Vector3{}]
   def ray_trace() do
-    for y <- 0..(@height - 1), x <- 0..(@width - 1) do
-      if x == 0 do
+    rows =
+      for y <- 0..(@height - 1) do
         IO.puts("#{y + 1} / #{@height}")
+
+        tasks =
+          for x <- 0..(@width - 1) do
+            Task.async(ElixirRayTracing, :ray_trace_pixel, [x, y])
+          end
+
+        for task <- tasks do
+          Task.await(task)
+        end
       end
 
-      accumulated_pixel_color =
-        Enum.reduce(1..@samples_per_pixel, %Vector3{}, fn _, color ->
-          Vector3.vector_add(color, calculate_pixel_color(x, y))
-        end)
+    List.flatten(rows)
+  end
 
-      Vector3.vector_mul_scalar(accumulated_pixel_color, 1.0 / @samples_per_pixel)
-    end
+  @spec ray_trace_pixel(integer(), integer()) :: %Vector3{}
+  def ray_trace_pixel(x, y) do
+    accumulated_pixel_color =
+      Enum.reduce(1..@samples_per_pixel, %Vector3{}, fn _, color ->
+        Vector3.vector_add(color, calculate_pixel_color(x, y))
+      end)
+
+    Vector3.vector_mul_scalar(accumulated_pixel_color, 1.0 / @samples_per_pixel)
   end
 
   @spec calculate_pixel_color(integer(), integer()) :: %Vector3{}
--- lib/image.ex
+++ lib/image.ex
@@ -2,16 +2,17 @@ require File
 require Pngex
 
 defmodule Image do
-  @spec save_as_png(String, integer(), integer(), [%Vector3{}]) :: none()
+  @spec save_as_png(Path.t(), integer(), integer(), [%Vector3{}]) :: :ok
   def(save_as_png(path, width, height, pixels)) do
     bitmap =
       for pixel <- pixels do
-        pixel_color = Vector3.vector_sqrt(pixel) |> Vector3.vector_mul_scalar(0xFF)
+        pixel_color = Vector3.vector_sqrt(pixel) |> Vector3.vector_mul_scalar(255.0)
         {trunc(pixel_color.x), trunc(pixel_color.y), trunc(pixel_color.z)}
       end
 
     image = generate_png(bitmap, width, height)
     File.write(path, image)
+    :ok
   end
 
   defp generate_png(bitmap, width, height) do
